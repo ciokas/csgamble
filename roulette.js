55ccca8ea4520c4e1b60f9157cb04eefbd31bbd2
ciokas <mserpenskas4@gmail.com>
2026-01-03 03:04:44 +0200

    Update roulette.js

--- roulette.js
+++ roulette.js
@@ -1,1 +1,11 @@
+// roulette.js
+function playRoulette(color) {
+  const roll = Math.random();
+  let result;
+  if (roll < 0.48) result = "Red";
+  else if (roll < 0.96) result = "Black";
+  else result = "Green";
 
+  if(result === color) alert("You won! The ball landed on " + result);
+  else alert("You lost! The ball landed on " + result);
+}
